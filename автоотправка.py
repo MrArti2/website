7ba2c804920f0c00ec1075a18a4bf7029810eb98
Pon <142164565+MrArti2@users.noreply.github.com>
2024-02-29 20:39:05 +0300

    Update автоотправка.py

--- автоотправка.py
+++ автоотправка.py
@@ -1,1 +1,28 @@
-чоо
+# ---------------------------------------------------------------------------------
+#  /\_/\  🌐 This module was loaded through https://t.me/hikkamods_bot
+# ( o.o )  🔐 Licensed under the GNU GPLv3.
+#  > ^ <   ⚠️ Owner of heta.hikariatama.ru doesn't take any responsibilities or intellectual property rights regarding this script
+# ---------------------------------------------------------------------------------
+# Name: afk
+# Description: Выдает сообщение о том, что вы недоступны.
+# Author: Агент
+# Commands:
+# .afk | .unafk
+# ---------------------------------------------------------------------------------
+
+
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
